b7c5815f0282205c0760f28230febaf7c0f7c8cc
DHANUSH6626 <dhanush13122000@gmail.com>
2023-09-25 14:07:45 +0530

    push

---
+++ Day7 (Review)/Online admission/src/Pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar1 from '../Components/Admin/Navbar1'
+
+function Dashboard() {
+  return (
+    <div>
+        <Navbar1/>
+        
+    </div>
+  )
+}
+
+export default Dashboard;
